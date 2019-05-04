4b5b20fd9668a4396bd2cd6231cc54b8379a4845
kozeljko <ak6689@student.uni-lj.si>
2019-05-04 23:34:58 +0200

    Fixed some regex patterns, simplified the clean up method

--- src/main/java/si/fri/kozelj/parsers/Parser.java
+++ src/main/java/si/fri/kozelj/parsers/Parser.java
@@ -2,4 +2,8 @@ package si.fri.kozelj.parsers;
 
 public interface Parser {
     String parseJson();
+
+    default String cleanMatch(String match) {
+        return match.replaceAll("\\n", " ").replaceAll("[\\t]", "").trim();
+    }
 }
